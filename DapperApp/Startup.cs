202154fdc010c28cffbf0b558503e9805e303db4
kubila <dolaskyasin@gmail.com>
2021-02-16 02:55:33 +0300

    Create DbContext and add to Startup.cs

--- DapperApp/Startup.cs
+++ DapperApp/Startup.cs
@@ -1,6 +1,8 @@
+using DapperApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace DapperApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(c =>
+            {
+                c.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
