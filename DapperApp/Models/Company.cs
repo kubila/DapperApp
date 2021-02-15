c066b21717d40392a786e6bc9b5d8199eb3ba5d2
kubila <dolaskyasin@gmail.com>
2021-02-16 02:28:47 +0300

    Create Company model, add ConnectionString

---
+++ DapperApp/Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperApp.Models
+{
+    public class Company
+    {        
+        public int CompanyId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+
+    }
+}
