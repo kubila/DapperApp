33a4feed5ae48430f71b672f672a7b9e0d1b758b
kubila <dolaskyasin@gmail.com>
2021-02-18 04:21:35 +0300

    Add Dapper, create new Repository for it

---
+++ DapperApp/Repository/CompanyRepository.cs
@@ -0,0 +1,46 @@
+﻿using DapperApp.Data;
+using DapperApp.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperApp.Repository
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        public IDbConnection _db;   
+        public CompanyRepository(IConfiguration configuration)
+        {
+            _db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public Company Add(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Company> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            
+        }
+
+        public Company Update(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
