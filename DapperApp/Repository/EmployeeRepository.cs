e5728822a49a45d421837b75492c09e5d7984e9f
kubila <dolaskyasin@gmail.com>
2021-02-21 23:42:28 +0300

    Create EmployeeRepository

---
+++ DapperApp/Repository/EmployeeRepository.cs
@@ -0,0 +1,45 @@
+﻿using DapperApp.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperApp.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private IDbConnection _db;
+        public EmployeeRepository(IConfiguration configuration)
+        {
+            _db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public Employee Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GeAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
